20a3e7c0ca3189d950acc7c4a21a033649cb02d9
Aman <aman01jain@gmail.com>
2024-12-19 01:17:57 -0700

    Send Emails using the forms

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from send_email import email_send
 
 st.header("Contact Me")
+
+with st.form(key="Form"):
+    user_email = st.text_input("Enter Email Address")
+    user_text = st.text_area("Message")
+    message = f"""\
+Subject : New email from {user_email}
+
+From : {user_email}
+{user_text}
+"""
+    submit = st.form_submit_button()
+    if submit:
+        email_send(message)
+        st.info("Email sent successfully")
