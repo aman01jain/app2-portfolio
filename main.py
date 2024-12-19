4ca45262746da83b8bf57187a59dde0de99e8129
Aman <aman01jain@gmail.com>
2024-12-18 23:48:16 -0700

    Render the titles in two columns

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg")
+    st.image("images/photo.jpg", width=450)
 with col2:
     st.title("Aman Jain")
     content = """
@@ -14,3 +15,20 @@ with col2:
     Eager to advance expertise
     """
     st.write(content)
+
+content2 = """
+    Below you can find the details of my contact 
+"""
+
+st.write(content2)
+
+df = pd.read_csv("data.csv", sep=";")
+col3, col4 = st.columns(2)
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:20].iterrows():
+        st.header(row["title"])
